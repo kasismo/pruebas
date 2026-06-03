655453b0496fbc3fc8f3df65a01ccedce8865874
kasismo <fakuokey@gmail.com>
2026-06-03 09:47:46 -0300

    Add files via upload

---
+++ prueba_streamlit.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.title("Prueba De Streamlit")
+
+st.header("Somos Streamlit y ésto es lo que podemos hacer")
+
+st.subheader("Titulos, interfaz, letras en negrita y mucho más")
+
+st.caption("Facil y practico para aquellos que se centran en el backend")
+
+st.write("Esto es una prueba muy iconica de lo que se puede hacer con Streamlit")
+
+st.markdown("Y la verdad es que está muy bueno 🎈")
+
+
